978e2ec1e4c25d7bf2d9d4a5930879fb7dd8fd9c
agkosd <sonawaneakshay039@gmail.com>
2022-03-03 15:31:19 +1100

    Add Single Select and Multi Select Components

---
+++ src/Components/Select/MultiSelect.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { useState } from "react";
+
+interface MultiSelectProps {
+  currencyKeys: string[];
+}
+
+const MultiSelect = ({ currencyKeys }: MultiSelectProps) => {
+  const [currency, setCurrency] = useState<string[]>([]);
+
+  const handleMultipleSelectChange = (
+    event: SelectChangeEvent<typeof currency>
+  ) => {
+    const {
+      target: { value }
+    } = event;
+    setCurrency(typeof value === "string" ? value.split(",") : value);
+  };
+  return (
+    <FormControl fullWidth>
+      <InputLabel id="currency-select-list-label">Currency List</InputLabel>
+      <Select
+        labelId="currency-select-list-label"
+        id="currency-select-list"
+        label="Curreny List"
+        value={currency}
+        multiple={true}
+        onChange={handleMultipleSelectChange}
+      >
+        {currencyKeys.map((currencyKey, idx) => (
+          <MenuItem value={currencyKey} key={currencyKey}>
+            {currencyKey}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default MultiSelect;
---
+++ src/Components/Select/SingleSelect.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { useState } from "react";
+
+interface CustomSelectProps {
+  currencyKeys: string[];
+  isMultipleSelect?: boolean;
+}
+
+const CustomSelect = ({
+  currencyKeys,
+  isMultipleSelect
+}: CustomSelectProps) => {
+  const [currency, setCurrency] = useState<string>("");
+
+  const handleSingleSelectChange = (event: SelectChangeEvent) => {
+    const value = event.target.value as string;
+    setCurrency(value);
+  };
+
+  return (
+    <FormControl fullWidth>
+      <InputLabel id="currency-select-list-label">Currency List</InputLabel>
+      <Select
+        labelId="currency-select-list-label"
+        id="currency-select-list"
+        label="Curreny List"
+        value={currency}
+        multiple={isMultipleSelect}
+        onChange={handleSingleSelectChange}
+      >
+        {currencyKeys.map((currencyKey, idx) => (
+          <MenuItem value={currencyKey} key={currencyKey}>
+            {currencyKey}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default CustomSelect;
