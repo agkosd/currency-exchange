6a5edb48a0aab71deb0185236a079c1a75382df1
agkosd <sonawaneakshay039@gmail.com>
2022-03-06 17:38:15 +1100

    Add result component and tests

---
+++ src/Components/Result/index.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
+
+interface ResultObj {
+  from: string;
+  to: string;
+  amount: number;
+  result: number;
+}
+
+interface ResultProps {
+  results: Array<ResultObj>;
+}
+
+const Result = ({ results }: ResultProps) => {
+  if (!results.length) {
+    return <Typography variant="h6">Search to get results</Typography>;
+  }
+  return (
+    <Paper elevation={3}>
+      {results.map(({ from, to, amount, result }, idx) => (
+        <Box
+          sx={{ padding: '2rem' }}
+          key={`${from}-${idx}-${to}`}
+        >{`${amount}${from} = ${result}${to}`}</Box>
+      ))}
+    </Paper>
+  );
+};
+
+export default Result;
